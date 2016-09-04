c20f25e5433dcf9434c44a1b7e776bb19a0fa55b
Arnelle Balane <arnellebalane@gmail.com>
2016-09-04 17:37:06 +0800

    change winston.info calls to debug and error

--- routes.js
+++ routes.js
@@ -15,15 +15,15 @@ let router = new express.Router();
  *  not, we send a HTTP 403 response.
  **/
 router.get('/webhook', (req, res) => {
-    winston.info('Received webhook verification request');
+    winston.debug('Received webhook verification request');
     winston.debug(`hub.verify_token: ${req.query['hub.verify_token']}`);
     winston.debug(`hub.challenge: ${req.query['hub.challenge']}`);
 
     if (req.query['hub.verify_token'] === config.get('FB_VERIFY_TOKEN')) {
-        winston.info('Webhook verified. Sending back challenge value.');
+        winston.debug('Webhook verified. Sending back challenge value.');
         res.send(req.query['hub.challenge']);
     } else {
-        winston.info('Webhook verification failed.');
+        winston.error('Webhook verification failed.');
         res.sendStatus(403);
     }
 });
