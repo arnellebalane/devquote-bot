870cb1d1c5425bc446bd798d507774159f6aa8bc
Arnelle Balane <arnellebalane@gmail.com>
2016-09-04 16:04:25 +0800

    comment for get /webhook route

--- routes.js
+++ routes.js
@@ -6,6 +6,14 @@ let config = require('./config');
 let router = new express.Router();
 
 
+/**
+ *  GET /webhook
+ *
+ *  Handle messenger webhook verification. Facebook messenger will pass two
+ *  parameters: "verify_token" and "challenge". We check if the "verify_token"
+ *  is the value that we set it to and respond with the given "challenge". If
+ *  not, we send a HTTP 403 response.
+ **/
 router.get('/webhook', (req, res) => {
     winston.info('Received webhook verification request');
     winston.debug(`hub.verify_token: ${req.query['hub.verify_token']}`);
