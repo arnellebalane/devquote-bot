cdf0428cdbb6f07709377ad5cefb76f5dbb511fe
Arnelle Balane <arnellebalane@gmail.com>
2016-09-04 20:34:32 +0800

    fix some comment docs

--- routes.js
+++ routes.js
@@ -10,7 +10,7 @@ let router = new express.Router();
 /**
  *  GET /webhook
  *
- *  Handle messenger webhook verification. Facebook Messenger will pass two
+ *  Handle messenger webhook verification. Messenger Platform will pass two
  *  parameters: `verify_token` and `challenge`. We check if the `verify_token`
  *  is the value that we set it to and respond with the given `challenge`. If
  *  not, we send an HTTP 403 response.
@@ -33,8 +33,8 @@ router.get('/webhook', (req, res) => {
 /**
  *  POST /webhook
  *
- *  All callbacks from Facebook Messenger will be made to this route. For the
- *  format of the request body, check the "Common Format" section in this page:
+ *  All callbacks from Messenger Platform will be made to this route. For the
+ *  format of the request body, check out its documentation in this page:
  *  https://developers.facebook.com/docs/messenger-platform/webhook-reference
  **/
 
@@ -44,22 +44,19 @@ router.post('/webhook', (req, res) => {
     if (data.object === 'page') {
         /**
          *  Each item in `data.entry` corresponds to a page that our app is
-         *  subscribed to. We iterate over it because it is possible to
-         *  receive callbacks from multiple pages at once when Facebook
-         *  Messenger sends them as a batch.
+         *  subscribed to wherein some messaging events happened. We iterate
+         *  over it because it is possible to receive callbacks from multiple
+         *  pages at once when the Messenger Platform sends them as a batch.
          **/
         data.entry.forEach(entry => {
             let { id, time } = entry;
             logger.debug(`Received webhook callback from page ${id} at ${time}.`);
 
             /**
-             *  Each item in `entry.messaging` corresponds to messages that
-             *  we sent to our app. Each message has a different format based
-             *  on what type of callback it is. Documentation for each callback
-             *  can be found at https://developers.facebook.com/docs/messenger-platform/webhook-reference
-             *
-             *  We then handle these messages differently depending on what
-             *  type of message they are.
+             *  Each item in `entry.messaging` corresponds to a messaging event
+             *  that involved the page (e.g. a message was sent to the page).
+             *  We handle these messages differently depending on what type of
+             *  message they are.
              **/
             entry.messaging.forEach(messaging => {
                 if (messaging.message && !messaging.message.is_echo) {
@@ -85,7 +82,7 @@ router.post('/webhook', (req, res) => {
 
         /**
          *  We need to send back an HTTP 200 response within 20 seconds to let
-         *  Facebook Messenger know that we've successfully received the
+         *  the Messenger Platform know that we've successfully received the
          *  callback. Otherwise, the request will time out.
          **/
         res.sendStatus(200);
